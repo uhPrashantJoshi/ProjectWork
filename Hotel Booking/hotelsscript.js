d04d3b4ea7075718d656132eecb0167a13c0cf6f
Prashant R Joshi <87548699+uhPrashantJoshi@users.noreply.github.com>
2022-03-03 11:21:25 +0530

    Add files via upload

---
+++ Hotel Booking/hotelsscript.js
@@ -0,0 +1,26 @@
+
+
+
+function jaipur(){
+   
+    document.getElementById("bd").style.backgroundImage="url(jaipur.jpg)";
+}
+
+function tokyo(){
+ 
+    document.getElementById("bd").style.backgroundImage="url(tokyo.jpg)";
+}
+
+function melbourne(){
+    document.getElementById("bd").style.backgroundImage="url(melbourne.jpg)";
+}
+
+function newyork(){
+    document.getElementById("bd").style.backgroundImage="url(newyork.jpg)";
+}
+
+
+function cairo(){
+    document.getElementById("bd").style.backgroundImage="url(cairo.jpg)";
+}
+
